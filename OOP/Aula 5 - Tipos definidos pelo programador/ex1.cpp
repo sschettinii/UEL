0b0819219203c65427beb6dbbb6560648eaea088
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-08 11:10:01 -0300

    Update ex1.cpp

--- OOP/Aula 5 - Tipos definidos pelo programador/ex1.cpp
+++ OOP/Aula 5 - Tipos definidos pelo programador/ex1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+using namespace std;
+
+struct notes{
+    int vec_notas[4];
+};
+
+
+int main(){
+    int counter=0,soma = 0,nota_temp,media;
+
+    struct notes notas;
+
+    for (int i=0;i<4;i++){
+        cout << "Nota " << i << ": ";
+        cin >> nota_temp;
+        notas.vec_notas[i] = nota_temp;
+    }
+
+    for (int i=0;i<4;i++){
+        soma += notas.vec_notas[i];
+    }
+    media = soma/4;
+    
+    if (media<4){
+        cout << "Reprovado";
+    }
+    if (media >= 4 && media<6){
+        cout << "Exame";
+    }
+    if (media >= 6){
+        cout << "Aprovado";
+    }
+}
