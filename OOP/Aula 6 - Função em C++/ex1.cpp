ef9280a5d31e4abb23cde96c1b5b7e20505b0856
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-08 17:35:43 -0300

    Update ex1.cpp

--- OOP/Aula 6 - Função em C++/ex1.cpp
+++ OOP/Aula 6 - Função em C++/ex1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int LenArr(int variable_arr[]){
+    int resultado;
+
+    while()
+
+    return resultado;
+}
+
+int main(){
+    int elemento,arr[],counter=0;
+    cout << "Digite os elementos do array: ";
+    cin >> elemento;
+    
+}
