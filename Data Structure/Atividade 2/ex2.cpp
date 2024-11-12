37e5a9859b6c6d4dc69ec9866a0cc87a96067147
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-12 11:55:20 -0300

    Update ex2.cpp

--- Data Structure/Atividade 2/ex2.cpp
+++ Data Structure/Atividade 2/ex2.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+
+    int mat_elemento,matriz[3][3];
+
+    //Recebe os elementos da matriz
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++){
+            cout << "Digite o valor da matriz[" << i+1 << "][" << j+1 << "]: "; 
+            cin >> mat_elemento;
+            matriz[i][j] = mat_elemento;
+        }
+    
+    //Printa a matriz
+    for(int i=0;i<3;i++){
+        cout << "\n";
+        for(int j=0;j<3;j++){
+            cout << matriz[i][j];
+        }
+    }
+
+    //Verifica a simetria da matriz
+    
+
+    return 0;
+
+}
