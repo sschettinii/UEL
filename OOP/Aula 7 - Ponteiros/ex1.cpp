a927ba14e75bc5fef78ac8c3dec939716ed9095c
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-12 20:41:17 -0300

    Update ex1.cpp

--- OOP/Aula 7 - Ponteiros/ex1.cpp
+++ OOP/Aula 7 - Ponteiros/ex1.cpp
@@ -1,1 +1,14 @@
+//Algoritmo que printa uma matriz acessada por ponteiros
+#include <iostream>
+using namespace std;
 
+int main() {
+    
+    int matriz[2][2] = {{1,2},{3,4}};
+    int* ponteiro;
+    ponteiro = &matriz[0][0];
+    
+    for (int i=0;i<4;i++){
+        cout << *(ponteiro+i);
+    }
+}
