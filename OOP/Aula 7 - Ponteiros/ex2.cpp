023d04e4fb19dadb991113407b8c7a649528f3a7
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-13 15:55:31 -0300

    Update ex2.cpp

--- OOP/Aula 7 - Ponteiros/ex2.cpp
+++ OOP/Aula 7 - Ponteiros/ex2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+void troca_valores(int*a,int*b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main(){
+    int x = 5, y = 10;
+    cout << "Antes das trocas: x = " << x << ", y = " << y << endl;
+
+    troca_valores(&x, &y);
+    cout << "Depois das trocas: x = " << x << ", y = " << y << endl;
+
+    return 0;
+}
