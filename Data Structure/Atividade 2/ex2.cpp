d9e0e084f6d132b76929771b83bebd6d2326e104
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-13 15:03:22 -0300

    Update ex2.cpp

--- Data Structure/Atividade 2/ex2.cpp
+++ Data Structure/Atividade 2/ex2.cpp
@@ -3,27 +3,46 @@ using namespace std;
 
 int main(){
 
-    int mat_elemento,matriz[3][3];
-
+    int mat_elemento,rows,columns;
+    //Cria a matriz
+    cout << "Digite a quantidade de linha(s) da matriz: ";
+    cin >> rows;
+    cout << "Digite a quantidade de coluna(s) da matriz: ";
+    cin >> columns;
+    int matriz[rows][columns];
+    
     //Recebe os elementos da matriz
-    for(int i=0;i<3;i++)
-        for(int j=0;j<3;j++){
+    for(int i=0;i<rows;i++)
+        for(int j=0;j<columns;j++){
             cout << "Digite o valor da matriz[" << i+1 << "][" << j+1 << "]: "; 
             cin >> mat_elemento;
             matriz[i][j] = mat_elemento;
         }
     
     //Printa a matriz
-    for(int i=0;i<3;i++){
+    for(int i=0;i<rows;i++){
         cout << "\n";
-        for(int j=0;j<3;j++){
-            cout << matriz[i][j];
+        for(int j=0;j<columns;j++){
+            cout << matriz[i][j] << " ";
         }
     }
 
     //Verifica a simetria da matriz
+    bool simetric = true;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            if(matriz[i][j] != matriz[j][i]){
+                simetric = false;
+            }
+        }
+    }
     
-
+    //Retorna resultado
+    if (simetric){
+        cout << endl << endl << "A matriz é simetrica!";
+    }
+    else {
+        cout << endl << endl << "A matriz nao e simetrica!";
+    }
     return 0;
-
 }
