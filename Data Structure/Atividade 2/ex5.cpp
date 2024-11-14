6f9e3ecaec2176b03fc7c3fa78e6ac51ab08eb68
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-14 17:52:08 -0300

    Update ex5.cpp

--- Data Structure/Atividade 2/ex5.cpp
+++ Data Structure/Atividade 2/ex5.cpp
@@ -9,21 +9,21 @@ int main(){
     cin >> rows1;
     cout << "Digite a quantidade de coluna(s) da matriz: ";
     cin >> columns1;
-    int matriz[rows1][columns1];
+    int matriz1[rows1][columns1];
     
     //Recebe os elementos da matriz 1
     for(int i=0;i<rows1;i++)
         for(int j=0;j<columns1;j++){
             cout << "Digite o valor da matriz1[" << i << "][" << j << "]: "; 
             cin >> mat_elemento;
-            matriz[i][j] = mat_elemento;
+            matriz1[i][j] = mat_elemento;
         }
     
     //Printa a matriz 1
     for(int i=0;i<rows1;i++){
         cout << "\n";
         for(int j=0;j<columns1;j++){
-            cout << matriz[i][j] << " ";
+            cout << matriz1[i][j] << " ";
         }
     }
     
@@ -60,7 +60,26 @@ int main(){
     }
     
     //Multiplica as matrizes
+    int matriz_produto[rows1][columns2],temp=0;
+    for (int i=0;i<rows1;i++){
+        for (int j=0;j<columns1;j++){
+            for (int k=0;k<rows2;k++){
+                temp += matriz1[i][j]*matriz2[j][k];
+            }
+        matriz_produto[i][j] = temp;
+        temp = 0;
+        }
+    }
+    
+    //Printa a matriz_produto
+    cout << "\n" << "\n";
+    cout << "Matriz Produto: " << endl;
+    for(int i=0;i<rows1;i++){
+        cout << "\n";
+        for(int j=0;j<columns2;j++){
+            cout << matriz_produto[i][j] << " ";
+        }
+    }
     
     return 0;
 }
-    
