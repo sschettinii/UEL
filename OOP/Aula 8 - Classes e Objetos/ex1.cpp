b1ef6eb5c7858def1ef95b712413f10f422621b3
schettinii <65085223+sschettinii@users.noreply.github.com>
2024-11-14 21:46:17 -0300

    Update ex1.cpp

--- OOP/Aula 8 - Classes e Objetos/ex1.cpp
+++ OOP/Aula 8 - Classes e Objetos/ex1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
 
+class Carros{
+  private:
+
+    string marca,modelo
+    float valor,km
+    int combustivel
+
+  public:
+
+    void init (string marca, string modelo, float valor, float km, int combustilvel){
+      this->marca=marca;
+      this->modelo=modelo;
+      this->valor=valor;
+      this->km=km;
+      this->combustivel=combustivel;
+    }
+
+    string get_marca(){
+      
+    }
+    string get_modelo(){
+      
+    }
+    float get_valor(){
+      
+    }
+    float get_km(){
+      
+    }
+    int get_combustivel(){
+      
+    }
+};
